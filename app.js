a8dffb8eb3d22718b22913c268e3036dde4a2982
Ferhat ER <lersmooth@gmail.com>
2023-05-28 23:20:13 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+//define products
+const products = [
+  {
+    name: "mouse-gold",
+    color: "#94792c",
+    title: "AI Mouse 1st 2.4G Wireless Mouse",
+  },
+  {
+    name: "mouse-green",
+    color: "#248723",
+    title: "AI Mouse 2nd 2.4G Wireless Mouse",
+  },
+  {
+    name: "mouse-red",
+    color: "#8d2e39",
+    title: "AI Mouse 3nd 2.4G Wireless Mouse",
+  },
+];
+
+//select prev - next buttons
+const navArrows = document.querySelectorAll(".nav-arrow ");
+console.log(navArrows);
